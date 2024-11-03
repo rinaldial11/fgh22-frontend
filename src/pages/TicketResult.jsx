20fcbcebe2eb113f5f162928fef50f094dd63f5f
rinaldial11 <rinaldilengkana44@gmail.com>
2024-11-03 17:36:55 +0700

    make ticket result page responsive

--- src/pages/TicketResult.jsx
+++ src/pages/TicketResult.jsx
@@ -10,21 +10,23 @@ function TicketResult() {
   return (
     <>
       <NavBar />
-      <main className="text-base flex bg-abumuda text-maintext">
-        <div className="relative w-3/5 h-[920px] bg-avengers bg-no-repeat bg-cover">
-          <div className="absolute gap-5 flex-col text-white px-28 pt-80 pb-60 bg-[#000000CC] w-full h-full flex ">
+      <main className="text-base flex lg:flex-row flex-col items-center gap-14 lg:gap-1 bg-abumuda text-maintext">
+        <div className="relative w-full lg:w-3/5 h-[920px] bg-avengers bg-no-repeat bg-cover">
+          <div className="absolute lg:text-left text-center gap-5 flex-col text-white px-6 lg:px-28 pt-80 pb-60 bg-[#000000CC] w-full h-full flex lg:items-start items-center ">
             <div className="w-72">
               <img src={Funtastix} alt="Funtastix" />
             </div>
-            <div className="text-5xl font-bold">Thankyou For Purchasing</div>
-            <div className="text-2xl text-[#FFFFFFB2] w-full max-w-xl">
+            <div className="text-3xl lg:text-5xl font-bold">
+              Thankyou For Purchasing
+            </div>
+            <div className="text-lg lg:text-2xl text-[#FFFFFFB2] w-full max-w-xl">
               Lorem ipsum dolor sit amet consectetur. Quam pretium pretium
               tempor integer sed magna et.
             </div>
             <div className="text-lg font-bold">Please Download Your Ticket</div>
           </div>
         </div>
-        <div className="flex gap-6 justify-center w-2/5 flex-col items-center">
+        <div className="flex gap-6 pb-16 justify-center w-2/5 flex-col items-center">
           <div className="bg-white rounded-lg w-80 px-6 py-8 max-w-2xl flex flex-col items-center gap-10">
             <div>
               <img src={Barcode} alt="" />
@@ -74,11 +76,11 @@ function TicketResult() {
             </div>
           </div>
           <div className="flex flex-col gap-3">
-            <button className="flex justify-center items-center gap-6 w-96 h-14 border border-secondtix text-secondtix rounded">
+            <button className="flex justify-center items-center gap-6 w-80 lg:w-96 h-14 border border-secondtix text-secondtix rounded">
               <GoDownload className="text-2xl" />
               Download
             </button>
-            <div className="w-96 h-14">
+            <div className="w-80 lg:w-96 h-14">
               <AnchorMain content="Done" page="/profile" />
             </div>
           </div>

18be43f600b03a77fee4676fc3e0fdb15b981272
rinaldial11 <rinaldilengkana44@gmail.com>
2024-11-05 16:11:59 +0700

    add validation input simulation on login page

--- src/components/FormSign.jsx
+++ src/components/FormSign.jsx
@@ -5,6 +5,11 @@ import { FiEyeOff } from "react-icons/fi";
 function FormSign() {
   const [type, setType] = React.useState("password");
   const [icon, setIcon] = React.useState(<FiEye />);
+  const [email, setEmail] = React.useState("");
+
+  function formSubmit(e) {
+    e.preventDefault;
+  }
 
   function hidePassword() {
     if (type === "password") {
@@ -17,37 +22,39 @@ function FormSign() {
   }
   return (
     <>
-      <div className="w-full flex flex-col gap-3">
-        <label htmlFor="email">Email</label>
-        <div className="w-full h-16">
-          <input
-            className="w-full h-full rounded outline-none px-6 border border-maintix placeholder-secondtix box-border"
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Enter your email"
-          />
+      <form>
+        <div className="w-full flex flex-col gap-3">
+          <label htmlFor="email">Email</label>
+          <div className="w-full h-16">
+            <input
+              className="w-full h-full rounded outline-none px-6 border border-maintix placeholder-secondtix box-border"
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+            />
+          </div>
         </div>
-      </div>
-      <div className="w-full flex flex-col gap-3">
-        <label htmlFor="email">Password</label>
-        <div className="relative flex w-full h-16">
-          <input
-            className="w-full h-full rounded outline-none px-6 border border-maintix placeholder-secondtix box-border"
-            id="password"
-            name="password"
-            type={type}
-            placeholder="Enter your password"
-          />
-          <button
-            type="button"
-            onClick={hidePassword}
-            className="absolute right-6 top-1/3 text-xl"
-          >
-            {icon}
-          </button>
+        <div className="w-full flex flex-col gap-3">
+          <label htmlFor="email">Password</label>
+          <div className="relative flex w-full h-16">
+            <input
+              className="w-full h-full rounded outline-none px-6 border border-maintix placeholder-secondtix box-border"
+              id="password"
+              name="password"
+              type={type}
+              placeholder="Enter your password"
+            />
+            <button
+              type="button"
+              onClick={hidePassword}
+              className="absolute right-6 top-1/3 text-xl"
+            >
+              {icon}
+            </button>
+          </div>
         </div>
-      </div>
+      </form>
     </>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,47 @@
 import React from "react";
 import ButtonMain from "../components/ButtonMain";
 import ButtonSos from "../components/ButtonSos";
-import FormSign from "../components/FormSign";
 import { Link } from "react-router-dom";
 import LogoBrand from "../components/LogoBrand";
+import { FiEye } from "react-icons/fi";
+import { FiEyeOff } from "react-icons/fi";
 
 function Login() {
+  const [type, setType] = React.useState("password");
+  const [icon, setIcon] = React.useState(<FiEye />);
+
+  function formSubmit(e) {
+    e.preventDefault();
+
+    const data = new FormData(e.target);
+    const email = data.get("email");
+    const password = data.get("password");
+
+    if (!email || !password) {
+      window.alert("form login harus diisi");
+      return;
+    }
+    if (email !== "rinaldi@gmail.com") {
+      window.alert("email belum terdaftar");
+      return;
+    }
+    if (password !== "1234") {
+      window.alert("password salah");
+      return;
+    }
+    window.open("/");
+  }
+
+  function hidePassword() {
+    if (type === "password") {
+      setType("text");
+      setIcon(<FiEyeOff />);
+    } else {
+      setType("password");
+      setIcon(<FiEye />);
+    }
+  }
+
   React.useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -23,17 +59,48 @@ function Login() {
                 Sign in with your data that you entered during your registration
               </div>
             </div>
-            <form action="" className="flex flex-col gap-6">
-              <FormSign />
+            <form
+              action=""
+              onSubmit={formSubmit}
+              className="flex flex-col gap-6"
+            >
+              <div className="w-full flex flex-col gap-3">
+                <label htmlFor="email">Email</label>
+                <div className="w-full h-16">
+                  <input
+                    className="w-full h-full rounded outline-none px-6 border border-maintix placeholder-secondtix box-border"
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Enter your email"
+                  />
+                </div>
+              </div>
+              <div className="w-full flex flex-col gap-3">
+                <label htmlFor="email">Password</label>
+                <div className="relative flex w-full h-16">
+                  <input
+                    className="w-full h-full rounded outline-none px-6 border border-maintix placeholder-secondtix box-border"
+                    id="password"
+                    name="password"
+                    type={type}
+                    placeholder="Enter your password"
+                  />
+                  <button
+                    type="button"
+                    onClick={hidePassword}
+                    className="absolute right-6 top-1/3 text-xl"
+                  >
+                    {icon}
+                  </button>
+                </div>
+              </div>
               <div className="w-full text-end text-birmud">
                 <a href="#">Forgot your password?</a>
               </div>
-              <div className="w-full h-16">
-                <Link to="/">
-                  {" "}
-                  <ButtonMain content={"Login"} />{" "}
-                </Link>
-              </div>
+              <button className="w-full h-16">
+                <ButtonMain content={"Login"} />
+              </button>
             </form>
 
             <div className="text-center">
